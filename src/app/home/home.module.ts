1ffe6201d43d4762d5872138a8725408be1a44e1
Ohad Shai <officialohadshai@gmail.com>
2022-02-21 16:25:36 +0200

    Arragned the project, and added initial loader.

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
